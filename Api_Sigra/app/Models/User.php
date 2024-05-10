87d35638f03e3c6a905ca5fc81e4b910a320132d
obregon-josh <jo46646@gmail.com>
2024-05-09 23:31:03 -0500

    Se agrego passport al api

--- Api_Sigra/app/Models/User.php
+++ Api_Sigra/app/Models/User.php
@@ -6,10 +6,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use Laravel\Passport\HasApiTokens;
+//COMFIGURAR ROL_PERMISO-pendiente
 class User extends Authenticatable
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, HasApiTokens;
 
     /**
      * The attributes that are mass assignable.
---
+++ Api_Sigra/routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Http\Controllers\Api\ApiController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+// Route::get('/user', function (Request $request) {
+//     return $request->user();
+// })->middleware('auth:api');
+
+//RUTAS PUBLICAS
+//Route::resource('index', ApiController::class);
+Route::get('/index', [ApiController::class, 'index']);
+Route::post('/login', [ApiController::class, 'login']);
+
+//RUTAS DE PARA USUARIOS AUTENTICADOS
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+
+});
+//RUTAS PROTEGIDAS, "prefix" => "users"
+Route::group(["middleware" => ["auth:api"], ], function(){
+    Route::get('/profile', [ApiController::class, 'profile']);
+    Route::get('/logout', [ApiController::class, 'logout']);
+    Route::post('/register', [ApiController::class, 'register']);
+});
